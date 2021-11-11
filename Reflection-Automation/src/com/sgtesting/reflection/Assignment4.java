f09968328dd394fdabc064557395f6dc7e212749
SarveshHavalagi <havalagisarvesh56@gmail.com>
2021-11-11 18:47:23 +0530

    reflection assignment

---
+++ Reflection-Automation/src/com/sgtesting/reflection/Assignment4.java
@@ -0,0 +1,140 @@
+package com.sgtesting.reflection;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment4 {
+	public static WebDriver oBrowser=null;
+	
+	public void launchBrowser()
+	{
+		try
+		{
+			System.setProperty("webdriver.chrome.driver","E:\\ExampleAutomation\\Automation\\Web-Automation\\Library\\driver\\chromedriver.exe");
+			oBrowser=new ChromeDriver();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void navigate()
+	{
+		try
+		{
+			oBrowser.navigate().to("http://localhost:82/login.do");
+			oBrowser.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void login()
+	{
+		try 
+		{
+		oBrowser.findElement(By.name("username")).sendKeys("admin");
+		oBrowser.findElement(By.name("pwd")).sendKeys("manager");
+		oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+		Thread.sleep(4000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void minimizeFlyoutWindow()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("gettingStartedShortcutsPanelId")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void createcustomer()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\'topnav\']/tbody/tr/td[3]/a/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'cpTreeBlock\']/div[2]/div[1]/div[2]/div/div[1]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("/html/body/div[14]/div[1]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'customerLightBox_nameField\']")).sendKeys("Custo");
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'customerLightBox_commitBtn\']/div")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void modifyCutomer()
+	{
+		try
+		{
+		oBrowser.findElement(By.xpath("//*[@id=\'topnav\']/tbody/tr[1]/td[3]/a/div[1]")).click();
+		Thread.sleep(2000);
+		oBrowser.findElement(By.xpath("//*[@id=\'cpTreeBlock\']/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+		Thread.sleep(2000);
+		oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[1]/div[3]/div/div[1]")).click();
+		Thread.sleep(2000);
+		oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[1]/div[3]/div/div[2]/input")).clear();
+		Thread.sleep(2000);
+		oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[1]/div[3]/div/div[2]/input")).sendKeys("Customer");
+		oBrowser.findElement(By.xpath("//*[@id=\'cpTreeBlock\']/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+		Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void deleteCustomer()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\'topnav\']/tbody/tr/td[3]/a/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'cpTreeBlock\']/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[1]/div[4]/div/div/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[4]/div/div[3]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.id("customerPanel_deleteConfirm_submitTitle")).click();
+			Thread.sleep(2000);
+			
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void logout()
+	{
+		try
+		{
+			oBrowser.findElement(By.linkText("Logout")).click();
+			Thread.sleep(4000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void closeApplication()
+	{
+		try
+		{
+			oBrowser.quit();
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+	
+}
